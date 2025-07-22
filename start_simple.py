a10c02ef64800529be9d6db3daef4a875a6ec177
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-07-22 21:07:36 +0300

    Add start_simple.py script skeleton and import statements

---
+++ start_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple backend startup script"""
+
+import os
+import sys
+from pathlib import Path
+
+# Add the current directory to Python path
+sys.path.insert(0, str(Path(__file__).parent))
